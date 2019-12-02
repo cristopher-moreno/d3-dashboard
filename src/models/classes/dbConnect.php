b06b47ffb3d57d1fdd5fee050e91ddbcf3a80218
ich-heisse-shiba <cristophermoreno92@gmail.com>
2019-12-01 22:34:53 -0500

    update

--- src/models/classes/dbConnect.php
+++ src/models/classes/dbConnect.php
@@ -5,7 +5,7 @@ class DbConnect
     protected $_db;
     public function __construct()
     {
-        $this->_db = new mysqli(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), getenv('MYSQL_DATABASE'));
+        $this->_db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
         if ($this->_db->connect_errno) {
             echo "Fallo al conectar a la base de datos " . $this->db->connect_errno;
             return;

f0e7a4a89980c897cdb0c3647ddb1a19ff860033
ich-heisse-shiba <cristophermoreno92@gmail.com>
2019-12-01 22:30:16 -0500

    update

--- restart.sh
+++ restart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker-compose down;
 docker-compose up -d;
+docker-compose ps;
 #docker logs -f mysql;
--- src/models/classes/config.php
+++ src/models/classes/config.php
@@ -1,7 +1,7 @@
 
 <?php
-define('DB_HOST', '172.20.0.20');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_NAME', 'ds7');
+define('DB_HOST', getenv('MYSQL_HOST'));
+define('DB_USER', getenv('MYSQL_USER'));
+define('DB_PASS', getenv('MYSQL_PASSWORD'));
+define('DB_NAME', getenv('MYSQL_DATABASE'));
 ?>

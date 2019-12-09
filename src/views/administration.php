2132945b1306af79e8783179bc18f84d589dc7a5
ich-heisse-shiba <cristophermoreno92@gmail.com>
2019-12-09 17:39:35 -0500

    update

--- src/views/administration.php
+++ src/views/administration.php
@@ -1,4 +1,14 @@
 <?php
+// ===============================
+// Autor: Cristopher Moreno
+// Fecha Creación: 20191208
+// Fecha Modificación: -
+// ===============================
+// Descripción:
+// Este script se encarga de lo siguiente:
+// - Crear CRUD del proyecto
+//==================================
+
 include("../models/classes/header.php");
 require_once("../models/classes/engineData.php");
 

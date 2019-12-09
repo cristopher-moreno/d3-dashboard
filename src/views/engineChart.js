b49898ebfee929138e900dfb718d4f8e68c15ef7
ich-heisse-shiba <cristophermoreno92@gmail.com>
2019-12-09 17:12:42 -0500

    update

--- src/views/engineChart.js
+++ src/views/engineChart.js
@@ -6,7 +6,7 @@
 // Descripción:
 // Este script se encarga de lo siguiente:
 // - Recibe .json
-// - Gererar gráfico lineal (Chart.js / D3.js)
+// - Gererar gráfico lineal + Punto Pronóstico mediante Regresión Lineal Simple (Chart.js / D3.js)
 //==================================
 
 document.addEventListener("DOMContentLoaded", function(event) {
